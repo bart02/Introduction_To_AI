3f53115fcc68798d65fcd7966f5e50415e4db17e
Artem Batalov <a@batalov.me>
2022-04-05 18:06:00 +0300

    output public

--- src/Output.java
+++ src/Output.java
@@ -2,21 +2,21 @@ import java.util.List;
 
 public class Output {
     private Environment env;
-    private String algo_name;
-    private String outcome;
-    private List<Point> path;
-    private double time;
+    public String algoName;
+    public String outcome;
+    public List<Point> path;
+    public double time;
 
-    public Output(Environment env, String algo_name, String outcome, List<Point> path, double time) {
+    public Output(Environment env, String algoName, String outcome, List<Point> path, double time) {
         this.env = env;
-        this.algo_name = algo_name;
+        this.algoName = algoName;
         this.outcome = outcome;
         this.path = path;
         this.time = time;
     }
 
     public void print() {
-        System.out.println("Algorithm: " + algo_name);
+        System.out.println("Algorithm: " + algoName);
         System.out.println("Outcome: " + outcome);
         System.out.println("Number of steps: " + path.size());
         System.out.println("Path: " + path);
@@ -27,7 +27,7 @@ public class Output {
     public String toString() {
         return "Output{" +
                 "env=" + env +
-                ", algo_name='" + algo_name + '\'' +
+                ", algo_name='" + algoName + '\'' +
                 ", outcome='" + outcome + '\'' +
                 ", path=" + path +
                 ", time=" + time +
